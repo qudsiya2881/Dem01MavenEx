0d9a9174e545cda455ce32c2fefda6a969c774d8
qudsiya2881 <iamqudsiyakounain12@gmail.com>
2022-04-28 13:19:41 +0530

    second commit

--- Demo1MavenEx/src/main/java/com/lti/demos/TestCalculator.java
+++ Demo1MavenEx/src/main/java/com/lti/demos/TestCalculator.java
@@ -8,7 +8,7 @@ public class TestCalculator {
 		System.out.println("addition is :"+r);
 	    int x=c.subNos(500, 200);
 	    System.out.println("substractrion is:"+x);
-	    System.out.println("hello");
+	    System.out.println("helloo");
 	}
 	
 
